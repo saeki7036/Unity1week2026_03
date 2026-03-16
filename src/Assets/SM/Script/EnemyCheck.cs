3371cd38c1a1cdc73e12413007f9a549f54322d1
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2026-03-16 16:40:50 +0900

    シーン移行スクリプト追加

---
+++ src/Assets/SM/Script/EnemyCheck.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyCheck : MonoBehaviour
+{
+    [SerializeField] List<GameObject> EnemyList;
+
+    [SerializeField] UnityEvent ClearEvent;
+
+    [SerializeField] bool checkFlag = true;
+
+    public void CheckStart()
+    {
+        checkFlag = false;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(checkFlag)
+            return;
+
+        //bool check = false;
+
+        for (int i = EnemyList.Count - 1; i >= 0; i--)
+        {
+            if (EnemyList[i] == null)
+            {
+                EnemyList.RemoveAt(i);
+            }
+            if (!EnemyList[i].activeSelf)
+            {
+                //check = true;
+                //break;
+                return;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        ClearEvent.Invoke();
+
+        checkFlag = true;
+    }
+}
