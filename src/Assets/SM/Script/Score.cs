2a93974241f0ddef7c0006dd29867f8d1bd019af
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2026-03-22 00:51:50 +0900

    スコア追加

---
+++ src/Assets/SM/Script/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using unityroom.Api;
+
+public class Score : MonoBehaviour
+{
+    static int score = 0;
+    
+    public static void ScoreReset() => score = 0;
+    public static void AddScore(int add) => score += add;
+
+    public static void SendScore()
+    {
+        UnityroomApiClient.Instance.SendScore(1,score,ScoreboardWriteMode.HighScoreDesc);
+    }
+}
